c1f70b59001f0cdd66b95854854fd8d656214add
gokhanyurtsever <73485076+gokhanyurtsever@users.noreply.github.com>
2024-04-19 22:24:20 +0300

    project setup added

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { ThemeProvider } from '@emotion/react'
+import lightTheme from './theme/light.tsx'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ThemeProvider theme={lightTheme}>
+    <App /> 
+    </ThemeProvider>
+    
+  </React.StrictMode>,
+)
