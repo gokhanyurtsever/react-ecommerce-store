d209238c1876532ebd5a4538a9c6403a4f4272dc
gokhanyurtsever <73485076+gokhanyurtsever@users.noreply.github.com>
2024-04-26 22:16:17 +0300

    features component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Features from "./components/Features"
 import Header from "./components/Header"
 import Navbar from "./components/Navbar"
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Navbar/>
       <Header />
+      <Features />
     </>
   )
 }
