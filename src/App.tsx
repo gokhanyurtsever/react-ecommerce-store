1847a8056bf55dce0aa42c2982226d1605e52f9d
gokhanyurtsever <73485076+gokhanyurtsever@users.noreply.github.com>
2024-05-12 18:37:47 +0300

    latest posts component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import Banner from "./components/Banner"
 import Features from "./components/Features"
 import Header from "./components/Header"
+import LatestPosts from "./components/LatestPost"
 import Navbar from "./components/Navbar"
 import ProductSlider from "./components/ProductSlider"
-import { iphones } from "./data"
+import { BlogPosts, iphones } from "./data"
 
 function App() {
   
@@ -16,6 +17,10 @@ function App() {
       <ProductSlider title="mobile & tablets" data={iphones} delay={7000}
        />
        <Banner />
+       <LatestPosts 
+       title="latest posts" 
+       posts={BlogPosts}
+       />
     </>
   )
 }
